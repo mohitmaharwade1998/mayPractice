9a75745c227bda8cb22b404ac0a2ca5c0a5f6b67
mohitmaharwade1998 <mohitmaharwade227@gmail.com>
2023-06-01 10:42:09 +0530

    varible

---
+++ js/variable.js
@@ -0,0 +1,36 @@
+// let
+let a;
+a=20;
+console.log(a);
+
+let k;
+k=20;
+k=48
+console.log(k);
+
+let b=30;
+console.log(b);
+
+// c=70;
+// let c;
+// console.log(c);
+
+ /// var
+ var e=50;
+ console.log(e);
+
+var f;
+f=78;
+console.log(f);
+
+d=70;
+var d;
+console.log(d);
+
+//cont
+
+const g=120;
+console.log(g);
+
+// const h;
+// h=40;
